f6beac8bfdb29e940d7f50e580492851ed79c75f
Lanph3re <lanph3re@gmail.com>
2021-12-03 23:55:21 +0900

    edit

--- GA.py
+++ GA.py
@@ -1,108 +1,108 @@
 import networkx as nx
 import random
 
+
 class GA:
-    def __init__(self, m):
-        self.first_population_num = 10
-        self.generation_num = 100
-        self.max_graphs_num = 50
-        self.select_rate = 0.6
+    def __init__(self, model):
+        self.model = model
+        self.rounds = 10
+        self.population_size = 100
+        self.selected_population_size = 50
         self.crossover_rate = 0.7
+        self.mutation_rate = 0.05
         self.mutation_limit = 2
-        self.model = m
-        self.mutation_cnt = []
 
     def crossover(self, p0, p1):
-        child = nx.create_empty_copy(p0)
-        p0_edges = set(p0.edges)
-        p1_edges = set(p1.edges)
-        only_p0_edges = p0_edges - p1_edges
-        only_p1_edges = p1_edges - p0_edges
-        common_edges = p0_edges & p1_edges
-        for u, v in common_edges:
-            child.add_edge(u, v)
-        
-        for u, v in only_p0_edges | only_p1_edges:
-            if random.random() < 0.5:
+        if random.random > self.crossover_rate:
+            return p0
+        else:
+            child = nx.create_empty_copy(p0)
+            p0_edges = set(p0.edges)
+            p1_edges = set(p1.edges)
+
+            for u, v in p0_edges & p1_edges:
                 child.add_edge(u, v)
-        
-        return child
+
+            for u, v in p0_edges - p1_edges:
+                if random.random() < 0.5:
+                    child.add_edge(u, v)
+
+            for u, v in p1_edges - p0_edges:
+                if random.random() < 0.5:
+                    child.add_edge(u, v)
+
+            return child
 
     def mutation(self, p):
         child = nx.Graph.copy(p)
-        s, e = random.sample(child.edges, 1)[0]
-        while self.mutation_cnt[s] > self.mutation_limit or self.mutation_cnt[e] > self.mutation_limit:
-            s, e = random.sample(child.edges, 1)[0]
+        for _ in range(self.mutation_limit):
+            if random.random() < self.mutation_rate:
+                u, v = random.sample(child.edges, 1)[0]
+                node = random.sample(child.nodes, 1)[0]
+                if random.random() < 0.5 and (u, node) not in child.edges:
+                    child.add_edge(u, node)
+                    child.remove_edge(u, v)
+                elif (node, v) not in child.edges:
+                    child.add_edge(node, v)
+                    child.remove_edge(u, v)
+                else:
+                    pass
 
-        while True:
-            new_e = random.sample(child.nodes, 1)[0]
-            if (s, new_e) not in child.edges and self.mutation_cnt[new_e] < self.mutation_limit:
-                child.remove_edge(s, e)
-                child.add_edge(s, new_e)
-                self.mutation_cnt[s] += 1
-                self.mutation_cnt[new_e] += 1
-                return child
+        return child
 
-    def generate_first_population(self, g):
-        graphs = [g]
-        while len(graphs) < self.first_population_num:
-            child = self.mutation(g)
-            graphs.append(child)
-        return graphs
-
+    def generate_breeding_population(self, g):
+        return [self.mutation(g) for _ in range(self.selected_population_size)]
 
     def generate(self, graphs):
-        max_index = len(graphs)
-        while len(graphs) < self.max_graphs_num:
-            # crossover
-            if random.random() < self.crossover_rate:
-                p0, p1 = random.sample(graphs[:max_index], 2)
-                child = self.crossover(p0, p1)
-                graphs.append(child)
-            # mutate
-            else:
-                p = random.sample(graphs[:max_index], 1)[0]
-                child = self.mutation(p)
-                graphs.append(child)
-        return graphs
+        population = [
+            self.crossover(random.sample(graphs, 2))
+            for _ in range(self.population_size)
+        ]
+        population = [self.mutation(g) for g in population]
+        return population
+
+    def get_label(self, g):
+        # TODO:
+        pass
 
     # using loss function of model
-    def get_fitness(self, g, l):
+    def get_fitness(self, g):
+        # TODO:
+        label = get_label(g)
         return random.random()
 
     # predict graph and compare the result
-    def is_adversarial(self, g, l):
-        # This is pseudocode
-        # pred = model.predict(g)
-        # adv_l = np.argmax(pred)
-        # return l != adv_l
-        return False
+    def is_adversarial(self, g, fitness):
+        return fitness > 0.8
 
-    def run(self, graph, label):
-        # generate first population
-        self.mutation_cnt = [0 for _ in range(len(graph.nodes))]
-        graphs = self.generate_first_population(graph)
-        graphs = self.generate(graphs)
-        for _ in range(self.generation_num):
-            graphs_with_fitness = []
-            for g in graphs:
-                f = self.get_fitness(g, label)
-                graphs_with_fitness.append((g, f))
-            graphs_with_fitness.sort(key=lambda x:x[1], reverse=True)
-            selected_graphs = list(map(lambda x:x[0],
-                graphs_with_fitness[:int(self.max_graphs_num * self.select_rate)]))
-            if self.is_adversarial(selected_graphs[0], label):
-                break
-            # generate new population
-            graphs = self.generate(selected_graphs)
+    def select(self, graphs):
+        graphs.sort(key=lambda x: x[1], reverse=True)
+        return graphs[:self.selected_population_size]
+
+    def run(self, graph):
+        graphs_with_fitness = self.generate_breeding_population(graph)
+        for _ in range(self.rounds):
+            graphs = [(g, self.get_fitness(g)) for g in self.generate(graphs)]
+            for g, fitness in graphs:
+                if self.is_adversarial(g, fitness):
+                    return g
+            else:
+                graphs = self.select(graphs)
+
+        return None
 
 
 if __name__ == '__main__':
+    # TODO: load dataset and model
+    model = None
+
     g = nx.Graph()
     for i in range(30):
         g.add_node(i)
+
     for _ in range(40):
         s, e = random.sample(range(30), 2)
         g.add_edge(s, e)
-    
-    GA(0).run(g, 0)
+
+    adversarial_graph = GA(model).run(g)
+    # TODO: print?
